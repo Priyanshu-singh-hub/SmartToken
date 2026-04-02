725e134ab44872a3ca2e8ca44ee77fae8f19638c
https://github.com/Priyanshu-singh-hub <singhpriyanshu2471@gmail.com>
2026-04-02 10:09:04 +0530

    fixed render deployment java 17

--- src/main/java/com/smarttoken/controller/TokenController.java
+++ src/main/java/com/smarttoken/controller/TokenController.java
@@ -1,17 +1,12 @@
 package com.smarttoken.controller;
 
 import com.smarttoken.entity.Token;
-import com.smarttoken.repository.TokenRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TokenController {
-
-    @Autowired
-    private TokenRepository tokenRepository;
 
     @GetMapping("/book")
     public String bookPage(Model model) {
@@ -21,19 +16,13 @@ public class TokenController {
 
     @PostMapping("/save")
     public String saveToken(@ModelAttribute Token token, Model model) {
-        Long count = tokenRepository.count();
-        token.setTokenNumber((int) (count + 1));
-
-        tokenRepository.save(token);
-
+        token.setTokenNumber(1); // temporary fixed token number
         model.addAttribute("tokenNumber", token.getTokenNumber());
-
         return "success";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteToken(@PathVariable Long id) {
-        tokenRepository.deleteById(id);
         return "redirect:/admin";
     }
 }
